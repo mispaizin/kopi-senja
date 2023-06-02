9d402f09dd923aa9f8887d05065b2de929f78dd2
mispaizin <mispaizin10@gmail.com>
2023-06-02 17:59:16 +0700

    mengaktifkan tombol search

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
-// toggle class active
+// toggle class active untuk hamburger mwnu
 const navbarNav = document.querySelector('.navbar-nav')
 // ketika tombol di klik
 document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active')
 }
+
+// toggle class active untuk search form
+const searchForm = document.querySelector('.search-form')
+const searchBox = document.querySelector('#search-box')
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active')
+    searchBox.focus()
+    e.preventDefault()
+}
+
 // klik di luar side bar untuk menghilangkan navnya
-const hamburger = document.querySelector('#hamburger-menu')
+const hm = document.querySelector('#hamburger-menu')
+const sb = document.querySelector('#search-button')
 
 document.addEventListener('click', function (e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active')
     }
 })
+
+document.addEventListener('click', function (e) {
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active')
+    }
+})
+
