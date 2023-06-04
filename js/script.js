38a8a267e085926f58c32b2746f0c9c505b21c8b
mispaizin <mispaizin10@gmail.com>
2023-06-04 18:15:38 +0700

    mengaktifkan kranjang belanja dan menambahkan produk

--- js/script.js
+++ js/script.js
@@ -9,15 +9,27 @@ document.querySelector('#hamburger-menu').onclick = () => {
 const searchForm = document.querySelector('.search-form')
 const searchBox = document.querySelector('#search-box')
 
+// ketika tombol di klik
 document.querySelector('#search-button').onclick = (e) => {
     searchForm.classList.toggle('active')
     searchBox.focus()
     e.preventDefault()
 }
 
-// klik di luar side bar untuk menghilangkan navnya
+// toggle class active untuk shopping cart
+const shoppingCart = document.querySelector('.shopping-cart')
+const cartItem = document.querySelector('.cart-item')
+
+// ketika tombol di klik
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    shoppingCart.classList.toggle('active')
+    e.preventDefault
+}
+
+// klik di luar untuk menghilangkan 
 const hm = document.querySelector('#hamburger-menu')
 const sb = document.querySelector('#search-button')
+const sc = document.querySelector('#shopping-cart-button')
 
 document.addEventListener('click', function (e) {
     if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -30,4 +42,12 @@ document.addEventListener('click', function (e) {
         searchForm.classList.remove('active')
     }
 })
+
+document.addEventListener('click', function (e) {
+    if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove('active')
+    }
+})
+
+
 
